c407fb9fa2a613525e60cefe5c50d470b8c53f0d
kshssafy16 <kshssafy16@gmail.com>
2026-07-15 14:43:04 +0900

    design: final polish (A) (#4)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
+  <div class="min-h-screen bg-gray-50 flex flex-col">
     <Header />
-    <main class="max-w-5xl mx-auto p-4">
-      <router-view />
+    <main class="container-max flex-1 p-4">
+      <div class="px-4 sm:px-6 lg:px-8">
+        <router-view />
+      </div>
     </main>
     <Footer />
     <Chatbot />
